a675b22df811680b94998d6d104cb745766368f7
KondwaniLaher <kondwanilaher@yahoo.com>
2020-06-11 15:12:47 +0100

    empty files set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Title from "./components/Title";
+import Items from "./components/Items";
+import Calculator from "./components/Calculator"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  
+}
+
+render (props) {
+  return ( 
+  <div className="App">
+    <Title/>
+    <Items/>
+    <Calculator/>
+  </div>)
+ 
 }
 
 export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <header>
+      <h1>Kondwani's totally excellent daily expenses calculator</h1>
+    </header>
+  );
+};
+
+export default Title;
